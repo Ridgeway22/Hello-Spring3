3a04d5193ae46c680265bf04a62136f3e861def0
Ridgeway22 <boo22mickey@yahoo.com>
2022-04-22 17:51:29 -0500

    git commit -m "1st"

--- src/main/java/org/launchcode/hellospring3/controller/hello.java
+++ src/main/java/org/launchcode/hellospring3/controller/hello.java
@@ -1,8 +1,15 @@
 package org.launchcode.hellospring3.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class hello {
 
+    @RequestMapping(value = "" +)
+    @ResponseBody
+    public String index(){
+        return "Hello world";
+    }
 }
